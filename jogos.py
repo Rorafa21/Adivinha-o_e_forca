40045403d7990e655c98d5d026a2f89af7717c25
Rodrigo Rafa <rodrigorafa71@gmail.com>
2022-02-02 21:03:57 -0300

    Jogos de Adivinhação e Forca

---
+++ jogos.py
@@ -0,0 +1,18 @@
+import forca
+import adivinhacao
+def escolher_jogo():
+    print("*********************************")
+    print("*******Escolha o seu jogo!*******")
+    print("*********************************")
+
+    print("(1)Forca , (2)Adivinhação")
+    jogo = int(input("Qual o jogo?"))
+
+    if(jogo == 1):
+        print("jogando forca")
+        forca.jogar()
+    elif(jogo == 2):
+        print ("jogando adivinhação")
+        adivinhacao.jogar()
+if (__name__ == "__main__"):
+    escolher_jogo()
